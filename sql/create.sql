92d48f4df73b373e4a55461ba7b6eee1d2d06e44
ToolMu <toolmumu@gmail.com>
2019-03-09 12:09:01 +0800

    add ip_channel to sql

--- sql/create.sql
+++ sql/create.sql
@@ -5,6 +5,7 @@ USE proxypool;
 CREATE TABLE proxyip (
   ip_id int  NOT NULL  AUTO_INCREMENT,
   ip_str varchar(32) NOT NULL,
+  ip_channel varchar(32) NOT NULL,
   ip_in_time varchar(64),
   PRIMARY KEY(ip_id)
 ) ENGINE=InnoDB;
